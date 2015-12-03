2fd417bf047eab1caff33913ec88034ee2250a40
David <daveey@asana.com>
2015-12-03 08:41:07 -0800

    Initial Commit

---
+++ Blimpie/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Blimpie
+//
+//  Created by David Braginsky on 12/3/15.
+//  Copyright © 2015 daveey. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Blimpie/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Blimpie
+//
+//  Created by David Braginsky on 12/3/15.
+//  Copyright © 2015 daveey. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
